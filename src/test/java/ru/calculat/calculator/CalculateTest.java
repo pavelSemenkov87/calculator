bb06552d971fac0a373e589e64856e86a1fa21cb
Pavel Semenkov <pavel.semenkov87@mail.ru>
2016-08-21 12:16:07 +0300

    Initial commit

---
+++ src/test/java/ru/calculat/calculator/CalculateTest.java
@@ -0,0 +1,31 @@
+package ru.calculat.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Павел on 21.08.2016.
+ */
+public class CalculateTest {
+
+    @Test
+    public void getAnsuer() throws Exception {
+        Calculate calculate = new Calculate();
+        assertEquals(null, calculate.getAnsuer("(5.89*(6+56))-6/0"));
+    }
+    /*все тесты работают
+    public void getAnsuer() throws Exception {
+        Calculate calculate = new Calculate();
+        assertEquals(null, calculate.getAnsuer("(5.89*(6+56))-r5"));
+    }
+    public void getAnsuer() throws Exception {
+        Calculate calculate = new Calculate();
+        assertEquals("363.98", calculate.getAnsuer("(5.89*(6+56))-6/5"));
+    }
+    public void getAnsuer() throws Exception {
+        Calculate calculate = new Calculate();
+        assertEquals("90.14", calculate.getAnsuer("(5.89*6+56)-6/5"));
+    }*/
+}
